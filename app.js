e3ed35ddf809803b078c8517663e58e23ed97095
Khulan <khulan.flwr524@gmail.com>
2021-01-19 20:56:42 +0800

    hold tovchiig ajillagaatai bolgono

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
-// shoog ali talaaraa buusniig hadgalah huvisagch heregtei 1-6 gesn sanamsarguigeer huvisagchid ni hadgalj ugnu. shoog shideh eventlistener.
+// roll btn-ii event listener. shoog ali talaaraa buusniig hadgalah huvisagch heregtei 1-6 gesn sanamsarguigeer huvisagchid ni hadgalj ugnu. shoog shideh eventlistener.
 document.querySelector(".btn-roll").addEventListener('click', function () {
       var diceNumber = Math.floor(Math.random() * 6) + 1;
       //alert(" shoo shidegdlee : " + diceNumber);
@@ -33,20 +33,49 @@ document.querySelector(".btn-roll").addEventListener('click', function () {
             document.getElementById("current-" + activePlayer).textContent = roundScore;
       } else {
             // 1 buusan tul toglogchiin eeljiig solij ugnu. onoog ni 0 bolgono
-            roundScore = 0;
-            document.getElementById("current-" + activePlayer).textContent = roundScore;
-
-            //Toglogchiin eeljiig solino
-            activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-
-            //ulaan tsegig shiljuuleh . classlist dr ni remove gd nemj add ged hasaj bolno toglle heregleh hergtei toglle ni acrive bval hasaad active bhgui bval nemeed yavaad bdg 
-            document.querySelector(".player-0-panel").classList.toggle("active");
-            document.querySelector(".player-1-panel").classList.toggle("active");
-
-            // shoog alga bolgono
-            diceDom.style.display = "none";
+          switchToNextPlayer();
       }
 });
+
+//hold buttonii event listener
+document.querySelector(".btn-hold").addEventListener("click", function() {
+      //ug toglogchiin tsugluulsan onoog global onoon dr nemj ugnu
+      scores[activePlayer] = scores[activePlayer] + roundScore;
+
+       // delgetsen deerhi onoog ni uurchlunu
+       document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+      //ug toglogch hojson esehiig shalgah
+      if(scores[activePlayer] >= 100) {
+            document.getElementById('name-' + activePlayer).textContent = " Winner !!!" ;
+            document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+            document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+      } else {
+            //toglogchiin eeljiig solih
+            switchToNextPlayer();
+      }
+});
+
+//shine togloom ehluuleh tovchnii event listener
+document.querySelector('.btn-new').addEventListener('click', function() {
+
+});
+
+// toglogchiin eeljig solino
+function switchToNextPlayer() {
+      roundScore = 0;
+      document.getElementById("current-" + activePlayer).textContent = roundScore;
+
+      //Toglogchiin eeljiig solino
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+      //ulaan tsegig shiljuuleh . classlist dr ni remove gd nemj add ged hasaj bolno toglle heregleh hergtei toglle ni acrive bval hasaad active bhgui bval nemeed yavaad bdg 
+      document.querySelector(".player-0-panel").classList.toggle("active");
+      document.querySelector(".player-1-panel").classList.toggle("active");
+
+      // shoog alga bolgono
+      diceDom.style.display = "none";
+}
 
 /* 
 document.querySelector("#score-0").textContent = dice; contentiin utgiig uurchluhud
