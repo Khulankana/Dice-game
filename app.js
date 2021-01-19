e40055f3f903f9cc83772adc50274539dcacefa1
Khulan <khulan.flwr524@gmail.com>
2021-01-19 20:22:23 +0800

    toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // toglogchiin eeljiig hadgalah huvisagch, 1r toglogchiig 0, 2r toglogchiig 1 gej temdegleye.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // toglogchiin ysugluulsan onoog hadgalah huvisagch. 2 onoog 1 massivt hadgalj uguh ni tohiromjtoi
 var scores = [0 , 0];
@@ -8,7 +8,6 @@ var scores = [0 , 0];
 var roundScore = 0;
 
 document.getElementById("score-0").textContent = "0";  // getElementbyId ni queryseectoroosoo iluu hurdan ajilladg.
-document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
@@ -16,12 +15,37 @@ document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
-// shoog ali talaaraa buusniig hadgalah huvisagch heregtei 1-6 gesn sanamsarguigeer huvisagchid ni hadgalj ugnu.
+// shoog ali talaaraa buusniig hadgalah huvisagch heregtei 1-6 gesn sanamsarguigeer huvisagchid ni hadgalj ugnu. shoog shideh eventlistener.
 document.querySelector(".btn-roll").addEventListener('click', function () {
       var diceNumber = Math.floor(Math.random() * 6) + 1;
       //alert(" shoo shidegdlee : " + diceNumber);
+
+      // zurgiig block bolgoj haragduulna
       diceDom.style.display = "block";
+
+      //1-6 sanamsargui toonii hargalzah shoog img iin src-d hargalzuulj ugch bna
       diceDom.src = "dice-" + diceNumber + ".png";
+
+      //buusan too ni 1s yalgaatai uyed idevhitei toglogchiin eeljiin onoog  nemeduulne.
+      if(diceNumber !== 1) {
+            //1s yalgaatai too buulaa
+            roundScore = roundScore + diceNumber;
+            document.getElementById("current-" + activePlayer).textContent = roundScore;
+      } else {
+            // 1 buusan tul toglogchiin eeljiig solij ugnu. onoog ni 0 bolgono
+            roundScore = 0;
+            document.getElementById("current-" + activePlayer).textContent = roundScore;
+
+            //Toglogchiin eeljiig solino
+            activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+            //ulaan tsegig shiljuuleh . classlist dr ni remove gd nemj add ged hasaj bolno toglle heregleh hergtei toglle ni acrive bval hasaad active bhgui bval nemeed yavaad bdg 
+            document.querySelector(".player-0-panel").classList.toggle("active");
+            document.querySelector(".player-1-panel").classList.toggle("active");
+
+            // shoog alga bolgono
+            diceDom.style.display = "none";
+      }
 });
 
 /* 
